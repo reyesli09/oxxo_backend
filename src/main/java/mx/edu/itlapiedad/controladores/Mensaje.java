83f3d147d2813e0eab6cdef77e430730a5674d7e
reyesli <you@example.com>
2021-05-03 23:03:32 -0500

    Configuro url de base de datos y nuevo controlador /devops/hola

---
+++ src/main/java/mx/edu/itlapiedad/controladores/Mensaje.java
@@ -0,0 +1,16 @@
+package mx.edu.itlapiedad.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devops")
+public class Mensaje {
+ 
+	@GetMapping("/hola")
+	public String mostrarHola() {
+		return "Hola Devops"; 
+	}
+
+}
